a6ed308eac2ba7af85e1cd05f054129a95940d32
Sinem Taşdemir <sinemtasdemir01@gmail.com>
2026-05-18 12:43:15 +0300

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="container mt-5">
+
+      <h1 className="text-center">
+        Project Management Dashboard
+      </h1>
+
+      <p className="text-center">
+        React CRUD Project
+      </p>
+
+    </div>
+  )
+}
+
+export default App
